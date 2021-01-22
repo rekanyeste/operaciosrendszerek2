ecb48590137f49ae09e54108253ae499c2abdb32
Nyeste Reka <rekabeka37@gmail.com>
2021-01-22 17:27:35 +0100

    Config.php edited

--- protected/config.php
+++ protected/config.php
@@ -7,9 +7,9 @@ define('DATABASE_CONTROLLER', PROTECTED_DIR.'database.php');
 define('USER_MANAGER', PROTECTED_DIR.'userManager.php');
 
 define('DB_TYPE', 'mysql');
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'cook');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_HOST', 'sql7.freemysqlhosting.net');
+define('DB_NAME', 'sql7388577');
+define('DB_USER', 'sql7388577');
+define('DB_PASS', 'u8bgwmQgRH');
 define('DB_CHARSET', 'utf8');
 ?>
